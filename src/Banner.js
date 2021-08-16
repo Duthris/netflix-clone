0d1a5c04b11358784b5207bd22bb6b20d0fcffb1
ADEM ANIL YALIMDEMİR <adem.yalimdemir@std.antalya.edu.tr>
2021-08-16 19:55:49 +0300

    16.08.2021 - Edited.

--- src/Banner.js
+++ src/Banner.js
@@ -33,7 +33,7 @@ export default function Banner() {
                 backgroundImage: `url(
                 "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
             )`,
-                backgroundPosition: "center center",
+
             }}
         >
 
